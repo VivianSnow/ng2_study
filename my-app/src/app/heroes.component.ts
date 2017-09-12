cad79d3a8eeaf085a850ecf43610b597e8138e85
VivianSnow <viviansnow@bupt.edu.cn>
2017-09-12 13:47:08 +0800

    修改使其能够正常运行

--- my-app/src/app/heroes.component.ts
+++ my-app/src/app/heroes.component.ts
@@ -1,12 +1,11 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Hero} from './hero';
 import {HeroService} from './hero.service';
-import {OnInit} from '@angular/core';
 
 @Component({
   selector: 'my-heroes',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css'],
   providers: [],
 })
 
